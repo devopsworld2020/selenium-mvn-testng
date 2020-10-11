1b5c9619e8e7be20c9a6248f8838c7779304a1bb
akash <pratap_the_knight@yahoo.com>
2020-10-11 01:36:42 -0400

    Test negative cases 01

---
+++ src/test/java/testcases/TestCase2.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	
+	@Test
+	public void validateTitles( ) {
+		
+		String expected_title = "Yahoo.com";
+		String actual_title = "Gmail.com";
+		
+/*		if(expected_title.equals(actual_title)) {
+			System.out.println("Test Case Passed");
+		}else {
+			System.out.println("Test Case Failed");
+		}*/
+		
+		Assert.assertEquals(actual_title, expected_title);
+		
+		
+	}
+		
+
+}
