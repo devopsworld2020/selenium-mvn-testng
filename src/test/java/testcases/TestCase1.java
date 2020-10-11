37ea0e1a72b77167a478c87ff8fad43d572f8a32
akash <pratap_the_knight@yahoo.com>
2020-10-11 00:53:28 -0400

    first commit

---
+++ src/test/java/testcases/TestCase1.java
@@ -0,0 +1,45 @@
+package testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@BeforeMethod
+	public void createDBConn( ) {
+		System.out.println("Create DB Connection");
+	}
+
+	@AfterMethod
+	public void closeDBConn( ) {
+		System.out.println("Closing DB Connection");
+	}	
+	
+	@BeforeMethod
+	public void launchBrowser( ) {
+		System.out.println("Launching Browser");
+	}
+
+	@AfterMethod
+	public void closeBrowser( ) {
+		System.out.println("Closing Browser");
+	}	
+	
+	@Test(priority=1)
+	public void doUserReg( ) {
+		System.out.println("Executing User Reg Test");
+	}	
+	
+	@Test(priority=2)
+	public void doLogin( ) {
+		System.out.println("Executing Login Test");
+	}
+	
+	@Test(priority=3)
+	public void checkPage( ) {
+		System.out.println("Check for the page opening");
+	}	
+
+
+}
